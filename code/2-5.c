60f02e9e0e6af15f54f33df51c5901194034c299
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-05-28 12:29:31 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -4,6 +4,15 @@ int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
 
+    FILE* fp_src = fopen(argv[1],"r");
+    FILE* fp_dst = fopen(argv[2],"w");
 
+    char a;
+
+    while ((a = fgetc(fp_src)) != EOF) {
+        fputc(a, fp_dst);
+    }
+
+    fclose(fp_src);
+    fclose(fp_dst);
 }
-
