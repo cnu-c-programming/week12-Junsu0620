abf46a6fb96b897f7fce12f961f1c95b30e9c5f0
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-05-28 12:15:43 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -11,13 +11,24 @@ int main(int argc, const char* argv[]) {
     int count = 0;
     Student students[64];
 
-
+     while (fscanf(fp, "%s %d", students[count].name, &students[count].score) == 2) {
+        count++;
+    }
 
     int max = 0;
     float avg = 0;
 
+    for (int i = 0; i < count; i++) {
 
+        if (students[i].score > max)
+            max = students[i].score;
 
+        avg += students[i].score;
+    }
+
+    if (count > 0)
+        avg /= count;
+    
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
 
@@ -25,4 +36,3 @@ int main(int argc, const char* argv[]) {
 
     return 0;
 }
-
