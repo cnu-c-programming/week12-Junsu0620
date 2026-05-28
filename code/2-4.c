6428b087fd4220b4347d2df1f99e1766d1aca6f4
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-05-28 12:58:17 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -7,18 +7,19 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
-    int freq[26] = { 0 };
+if (fp == NULL)
+        return 0;
+
+    int num[32] = {0};
     char ch;
 
     while ((ch = fgetc(fp)) != EOF) {
-
-        if (ch >= 'a' && ch <= 'z') {
-            freq[ch - 'a']++;
-        }
+        if (ch >= 'a' && ch <= 'z')
+            num[ch - 'a']++;
     }
 
     for (int i = 0; i < 26; i++) {
-        printf("%c : %d\n", 'a' + i, freq[i]);
+        printf("%c : %d\n", 'a' + i, num[i]);
     }
 
     fclose(fp);
