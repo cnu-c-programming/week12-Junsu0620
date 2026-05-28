823eec66b53d019e393525f115a5aed0a41c9575
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-05-28 20:35:39 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -13,13 +13,10 @@ void config_parser(Config* config_ptr) {
 
     FILE* fp = fopen("config.txt", "r");
 
-    if (fp == NULL)
-        return;
-
-    fscanf(fp, "InputFileName=%s", config_ptr->InputFileName);
-    fscanf(fp, "Options=%d", &config_ptr->Options);
-    fscanf(fp, "SectionName=%s", config_ptr->SectionName);
-    fscanf(fp, "Address=%llu", &config_ptr->Address);
+    fscanf(fp, "%*[^=]=%s", config_ptr->InputFileName);
+    fscanf(fp, "%*[^=]=%d", &config_ptr->Options);
+    fscanf(fp, "%*[^=]=%s", config_ptr->SectionName);
+    fscanf(fp, "%*[^=]=%llu", &config_ptr->Address);
 
     fclose(fp);
 }
