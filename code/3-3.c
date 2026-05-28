3ed4e2feeb1c8d70328cedaa5bbb339c7ff5064d
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-05-28 12:54:31 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -13,7 +13,7 @@ int main(int argc, const char* argv[]) {
     char buf_fp2[256] = {0};
 
     char* res_fp1 = fgets(buf_fp1, sizeof(buf_fp1), fp1);
-    char* res_fp2 = fgets(buf_fp2, sizeod(buf_fp2), fp2);
+    char* res_fp2 = fgets(buf_fp2, sizeof(buf_fp2), fp2);
 
     printf("fp1 %s %p %d %d\n", buf_fp1, res_fp1, ferror(fp1), feof(fp1));
     printf("fp2 %s %p %d %d\n", buf_fp2, res_fp2, ferror(fp2), feof(fp2));
