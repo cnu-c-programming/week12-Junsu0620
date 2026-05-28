208741f6974ef9b05bff86e929f8d50b3a4b6252
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-05-28 12:33:25 +0900

    Update 2-6.c

--- code/2-6.c
+++ code/2-6.c
@@ -8,8 +8,11 @@ int main(int argc, const char* argv[]) {
     FILE* fp = fopen(argv[1], "r");
     const char* target_str = argv[2];
 
-
+    char s[256];
+    
+    while (fgets(s, sizeof(s), fp))
+        if (strstr(s, target_str))
+            printf("%s", s);
     
     fclose(fp);
 }
-
